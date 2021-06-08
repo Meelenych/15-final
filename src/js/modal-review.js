1036ed8d2620c16e04075ad6b79d26769fbf5f0f
Meelenych <80649931+Meelenych@users.noreply.github.com>
2021-06-09 01:27:25 +0300

    Review's articles got stylish

--- src/js/modal-review.js
+++ src/js/modal-review.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[data-modal-opn]'),
+    closeModalBtn: document.querySelector('[data-modal-cls]'),
+    modal: document.querySelector('[data-mdl]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
